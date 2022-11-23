b49309bff9314d8e4d23d8e658952e2257ef3e53
anaalicefortuna <anaalicefalves@gmail.com>
2022-11-23 12:50:46 -0300

    page

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css'
+
+
+function Home() {
+  return (
+    <>
+    <div className='bg'>
+        <h1 className='titulo'>Web Design</h1>
+        <h2 className='sub'>LANDING PAGE</h2>
+
+        <img className='fundoimg' src="https://cdn.dribbble.com/users/1068771/screenshots/9805180/media/5dd8a906debc045b2d1beb1d9bc22fcf.gif" alt="" />
+    
+    </div>
+
+    </>
+  )
+}
+
+export default Home
